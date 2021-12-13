d8ed4fef2ee2d9cb3591b0932d0bcffaede34f97
antonio canongo <tony@wlan-t-206-115-dhcp.CSUChico.EDU>
2021-12-13 13:08:05 -0800

    routing updates

--- realTimeChatCrypto/realTimeChatCrypto/asgi.py
+++ realTimeChatCrypto/realTimeChatCrypto/asgi.py
@@ -7,6 +7,30 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
+'''
+framework comes from django channels tutorial 
+'''
+
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import chat.routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
+
+'''
+
 import os
 
 from django.core.asgi import get_asgi_application
@@ -14,3 +38,7 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realTimeChatCrypto.settings')
 
 application = get_asgi_application()
+
+
+
+'''
